68eeaacaa22413290933f3e305c5b1eb606a2f30
Lexiprom <mrbiznesss52@gmail.com>
2026-08-16 12:48:23 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE = "lexi-shell-v2";
+
+const SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches
+          .open(CACHE)
+          .then(cache => cache.put(event.request, copy));
+
+        return response;
+      })
+      .catch(() =>
+        caches
+          .match(event.request)
+          .then(
+            response =>
+              response ||
+              caches.match("./index.html")
+          )
+      )
+  );
+});
